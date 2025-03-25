149e50706b674bfc2cad1742bc0f55d615ee1640
Antoine07 <antoine.lucsko@wanadoo.fr>
2025-03-25 11:57:31 +0100

    ts

--- TypeScript/Example/index.ts
+++ TypeScript/Example/index.ts
@@ -1,34 +1,34 @@
-import {foo} from './utils';
+import { foo } from './utils';
 
 function add(a: number, b: number, info: string): number {
     return a + b;
-  }
-  
-  const result: number = add(3, 5, "addition");
+}
 
-  console.log(result);
+const result: number = add(3, 5, "addition");
 
-  foo();
+console.log(result);
 
-  let num: number[] = [1, 2, 3, 4];
+foo();
 
-  function sum(num :number[]): number{
+let num: number[] = [1, 2, 3, 4];
+
+function sum(num: number[]): number {
 
     return 1;
-  }
+}
 
-  // typage de tuple
+// typage de tuple
 let person: [string, number] = ["John", 30];
 
 let value: any = "Hello";
 value = 42; // Pas d'erreur, mais perd les avantages du typage
 
 function sayHello(name: string, age?: number): void {
-  if (age) {
-    console.log(`Bonjour, ${name}! Tu as ${age} ans.`);
-  } else {
-    console.log(`Bonjour, ${name}!`);
-  }
+    if (age) {
+        console.log(`Bonjour, ${name}! Tu as ${age} ans.`);
+    } else {
+        console.log(`Bonjour, ${name}!`);
+    }
 }
 
 sayHello('Alan');
@@ -37,25 +37,19 @@ sayHello('Sophie', 22);
 // typage avec des objets ou interfaces
 
 class Person {
-  name: string;
-  age: number;
-  address?: string; // Propriété optionnelle
+    constructor(public name: string, public age: number, public address?: string) { }
 }
 
 let user: Person = {
-  name: "Alice",
-  age: 28,
+    name: "Alice",
+    age: 28,
 };
 
-function setPerson(p : Person){
-  console.log(p)
+function setPerson(p: Person) {
+    console.log(p)
 }
 
-const p =  new Person;
-
-p.name = "Alan";
-p.age = 22;
-
+const p = new Person('Alan', 22);
 setPerson(p);
 
 // type simple
@@ -70,35 +64,33 @@ type User = {
     status: Status;
 };
 
-const u : User = {
-  id : 2,
-  username : "Alan",
-  status : "active"
+const u: User = {
+    id: 2,
+    username: "Alan",
+    status: "active"
 }
 
 // typage générique nous allons le voir plus loin
 type Pair<T> = [T, T];
 
-const pp : Pair<Person> = [new Person, new Person];
+const pp: Pair<Person> = [new Person('Alice', 19), new Person('Bob', 18)];
 
-const qq : Pair<number> = [1, 2];
+const qq: Pair<number> = [1, 2];
 
 // syntaxe pour définir des fonctions avec des typages génériques 
-function merge<T>(a : T, b : T): T[]{
-    const res : T[] = [];
+function merge<T>(a: T, b: T): T[] {
+    const res: T[] = [];
     res.push(a);
     res.push(b);
 
-    return res; 
+    return res;
 }
 
 //on fixe le typage à l'appelle de la fonction
-console.log( merge<string>("1", "2"));
-console.log( merge<number>(1, 2));
+console.log(merge<string>("1", "2"));
+console.log(merge<number>(1, 2));
 
-const p1 = new Person
-p1.name = "Albert";
+const p1 = new Person('Sophie', 17)
 
-const p2 = new Person
-p2.name = "Alice";
-console.log( merge<Person>(p1, p2));
+const p2 = new Person('Michel', 67)
+console.log(merge<Person>(p1, p2));
